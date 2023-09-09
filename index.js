836c5a016d1a8c90bb240e7e3b81617a90e1f837
Marcos Bonfadini <mbabonfadini@gmail.com>
2023-09-09 13:52:06 -0300

    Change: Using corsOptions

--- index.js
+++ index.js
@@ -10,9 +10,17 @@ const fs = require("fs");
 const https = require("https");
 const app = express();
 
+const whitelist = ['https://my-netflix-back.vercel.app/', 'https://localhost:4000/']
+
+const corsOptions = {
+    origin: 'https://localhost:4000', // Substitua pelo seu domínio frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
+    credentials: true, // Habilitar o uso de credenciais (cookies, cabeçalhos de autorização, etc.)
+  };
+
 require("dotenv").config();
 
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -39,7 +47,7 @@ if (process.env.NODE_ENV !== 'teste') {
     const PORT = process.env.PORT || 4000;
     https.createServer({
         key: process.env.SERVER_KEY,
-        cert: process.env.SERVER_CRT
+        cert: process.env.SERVER_CRT,
     },
         app
     ).listen(PORT, () => console.log(`Servidor rodando na porta https://localhost:${PORT}`));
