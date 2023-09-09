b6841a5ef9e97a826eef2aba5d58157117dab29c
Marcos Bonfadini <mbabonfadini@gmail.com>
2023-09-09 17:10:15 -0300

    Correction: Fixing Doc acess

--- src/middlewares/authDoc.js
+++ src/middlewares/authDoc.js
@@ -14,20 +14,81 @@ async function authDocProducao(req, res, next) {
         res.status(401).set('Content-Type', 'text/html');
         res.send(Buffer.from(`
 
-            <form method="post" style="
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Página com Fundo Preto</title>
+        </head>
+        <body 
+        style="
+        margin:0;
+        background-color: white; 
+        background-image:url('https://i0.wp.com/cdn2.tfx.company/images/clickwallpapers-OnePiece-4k-img1.jpg?w=880&ssl=1');
+        background-repeat: no-repeat;
+        background-size: 100%;
+        ">
+            <form 
+            method="post" 
+            style="
+            font-family: Trebuchet Ms;
             display: flex;
             align-items: center;
             justify-content: center;
-            width: 98vw;
-            height: 97vh;
-            background-color: #000; 
+            width: 100vw;
+            height: 100vh;
             color:#fff;
+            background-color: #00000090;"
             ">
-                <p style="color: red;">Senha Errada!</p>
-                <label for="password">Senha da documentação:</label>
-                <input type="password" name="password" id="password" />
-                <button type="submit">Entrar</button>
+            <div 
+            style="text-align: center;">
+            <h1
+            >My Netflix API App</h1>
+            <div 
+            style="
+            display: flex;
+            border-radius: 3rem;
+            border: 1px solid #000;
+            background-color: #00000090;
+            ">
+            <input 
+            type="password" 
+            name="password" 
+            id="password" 
+            placeholder="Doc password..."
+            style="
+            height: 30px;
+            width: 300px;
+            background-color: #f1f1f100;
+            border: none;
+            border-top-left-radius:3rem;
+            border-bottom-left-radius: 3rem;
+            padding: 1rem 2rem;
+            color: #fff;
+            font-family: Trebuchet Ms;
+            font-weight: bold;
+            font-size: 1.25rem;
+            " />
+            <button 
+            type="submit"
+            style="
+            border-top-right-radius:3rem;
+            border-bottom-right-radius: 3rem;
+            border: none;
+            padding: 1rem 2rem;
+            background-color: #00FF91;
+            font-family: Trebuchet Ms;
+            font-weight: bold;
+            font-size:1rem;
+            height: 62px;
+            "
+            >Entrar</button>
+            
+            </div>
+            </div>
+            
             </form>
+            </body>
+        </html>
         `))
     } else {
         // Usuario ainda não digitou a senha e está em modo produção
@@ -64,6 +125,7 @@ async function authDocProducao(req, res, next) {
         >My Netflix API App</h1>
         <div 
         style="
+        display: flex;
         border-radius: 3rem;
         border: 1px solid #000;
         background-color: #00000090;
